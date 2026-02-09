177e72b3aff6025bbcc475a602c443c03f77f812
aleyamayab <kingdom_ale@hotmail.com>
2026-02-08 22:14:39 -0600

    delete

--- azure-vm-minikube/environments/dev/main.tf
+++ azure-vm-minikube/environments/dev/main.tf
@@ -27,8 +27,6 @@ module "vm" {
   name           = "vm-dev-minikube"
   rg_name        = module.rg.name
   location       = var.location
-  subnet_id      = module.network.subnet_id
-  nsg_id         = module.nsg.id
   admin_user     = var.admin_user
   admin_password = var.admin_password
 }
